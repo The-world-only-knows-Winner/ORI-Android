7b6b5aa9f28f412093da014a5cd2d96973077668
Tmdhoon2 <hks026naver@gmail.com>
2023-09-05 23:11:55 +0900

    ⚡️ :: Color

---
+++ presentation/src/main/java/com/onlywin/designsystem/Color.kt
@@ -0,0 +1,40 @@
+package com.onlywin.designsystem
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+object Color {
+
+    @Stable
+    val Gray100 = Color(0xFFFFFFFF)
+
+    @Stable
+    val Gray200 = Color(0xFFF6F6F6)
+
+    @Stable
+    val Gray300 = Color(0xFFCECED1)
+
+    @Stable
+    val Gray400 = Color(0xFF8E8E9D)
+
+    @Stable
+    val Gray500 = Color(0xFF575767)
+
+    @Stable
+    val Gray600 = Color(0xFF23232D)
+
+    @Stable
+    val Gray700 = Color(0xFF121218)
+
+    @Stable
+    val Primary100 = Color(0xFFF9F724)
+
+    @Stable
+    val Primary300 = Color(0xFFFFB800)
+
+    @Stable
+    val Red = Color(0xFFF42F2F)
+
+    @Stable
+    val Green = Color(0xFF3BD023)
+}
