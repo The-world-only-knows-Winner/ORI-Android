f02f5dc23278507d1cb369a90743faa5eaf1aa39
정승훈 <hks026naver@gmail.com>
2023-09-15 19:49:17 +0900

    ⚡️ :: Splash dark theme 적용

--- presentation/src/main/java/com/onlywin/ori_android/feature/Splash.kt
+++ presentation/src/main/java/com/onlywin/ori_android/feature/Splash.kt
@@ -2,26 +2,45 @@ package com.onlywin.ori_android.feature
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.onlywin.designsystem.R
+import androidx.compose.ui.unit.dp
+import com.onlywin.designsystem.DuckTheme
+import com.onlywin.designsystem.fondation.typography.Heading3
+import com.onlywin.ori_android.R
 
 @Composable
 internal fun Splash() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DuckTheme.colors.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
         Image(
-            modifier = Modifier.fillMaxSize(0.6f),
-            painter = painterResource(id = R.drawable.bg_splash),
+            modifier = Modifier.size(
+                width = 92.dp,
+                height = 46.dp,
+            ),
+            painter = painterResource(id = R.drawable.ic_splash_duck),
             contentDescription = stringResource(R.string.content_description_image_splash),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Heading3(
+            text = stringResource(id = R.string.splash_image_description),
+            color = DuckTheme.colors.surfaceTint,
         )
     }
 }
