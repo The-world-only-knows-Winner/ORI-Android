83fd7356e26507bc01b390a067de7d752bab6e9f
Tmdhoon2 <hks026naver@gmail.com>
2023-09-16 08:58:24 +0900

    ⚡️ :: Onboarding screen

--- presentation/src/main/java/com/onlywin/ori_android/feature/onboarding/Onboarding.kt
+++ presentation/src/main/java/com/onlywin/ori_android/feature/onboarding/Onboarding.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
@@ -18,9 +19,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.onlywin.designsystem.button.DuckLargeButton
 import com.onlywin.designsystem.component.Indicator
+import com.onlywin.designsystem.fondation.typography.Body3
+import com.onlywin.designsystem.fondation.typography.Body4
+import com.onlywin.designsystem.fondation.typography.Heading2
 import com.onlywin.ori_android.R
 
 const val PAGE_COUNT = 3
@@ -31,9 +37,26 @@ private val onboardingDrawables = listOf(
     R.drawable.bg_onboarding_4,
 )
 
+private val onboardingTitles = listOf(
+    R.string.onboarding_description_title_1,
+    R.string.onboarding_description_title_2,
+    R.string.onboarding_description_title_3,
+    R.string.onboarding_description_title_4,
+)
+
+private val onboardingDescriptions = listOf(
+    R.string.onboarding_description_des_1,
+    R.string.onboarding_description_des_2,
+    R.string.onboarding_description_des_3,
+    R.string.onboarding_description_des_4,
+)
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-internal fun Onboarding() {
+internal fun Onboarding(
+    moveToSignUp: () -> Unit,
+    moveToSignIn: () -> Unit,
+) {
 
     val pagerState = rememberPagerState(pageCount = { PAGE_COUNT })
 
@@ -44,6 +67,24 @@ internal fun Onboarding() {
         OnboardingPager(pagerState)
         Spacer(modifier = Modifier.height(12.dp))
         PagerIndicator(currentPage = pagerState.currentPage)
+        Spacer(modifier = Modifier.height(24.dp))
+        Heading2(text = stringResource(onboardingTitles[pagerState.currentPage]))
+        Spacer(modifier = Modifier.height(4.dp))
+        Body3(text = stringResource(id = onboardingDescriptions[pagerState.currentPage]))
+        Spacer(modifier = Modifier.weight(1f))
+        Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+            DuckLargeButton(
+                text = stringResource(id = R.string.onboarding_start_with_new_account),
+                onClick = moveToSignUp,
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Body4(
+            text = stringResource(id = R.string.onboarding_start_with_original_account),
+            decoration = TextDecoration.Underline,
+            onClick = moveToSignIn,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
@@ -77,7 +118,9 @@ private fun PagerIndicator(currentPage: Int) {
 )
 @Composable
 private fun LightThemeOnboardingPreview() {
-    Onboarding()
+    Onboarding(moveToSignUp = { /*TODO*/ }) {
+        
+    }
 }
 
 @Preview(
@@ -87,5 +130,7 @@ private fun LightThemeOnboardingPreview() {
 )
 @Composable
 private fun DarkThemeOnboardingPreview() {
-    Onboarding()
+    Onboarding(moveToSignUp = { /*TODO*/ }) {
+        
+    }
 }
--- presentation/src/main/java/com/onlywin/ori_android/navigation/UserNavigation.kt
+++ presentation/src/main/java/com/onlywin/ori_android/navigation/UserNavigation.kt
@@ -4,14 +4,26 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.onlywin.ori_android.feature.Splash
+import com.onlywin.ori_android.feature.onboarding.Onboarding
 
-internal fun NavGraphBuilder.userNavigation() {
+internal fun NavGraphBuilder.userNavigation(
+    moveToSignUp: () -> Unit,
+    moveToSignIn: () -> Unit,
+) {
     navigation(
         route = NavigationRoute.User.route,
-        startDestination = NavigationRoute.User.Splash,
+        startDestination = NavigationRoute.User.Onboarding,
     ) {
         composable(NavigationRoute.User.Splash) {
             Splash()
         }
+
+        composable(NavigationRoute.User.Onboarding) {
+            Onboarding(
+                moveToSignUp = moveToSignUp,
+                moveToSignIn = moveToSignIn,
+            )
+        }
+
     }
 }
