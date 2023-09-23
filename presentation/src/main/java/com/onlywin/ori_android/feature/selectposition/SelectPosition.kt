ec0d7e274ae29a37cf2f9bda11736d5b66829bab
정승훈 <hks026naver@gmail.com>
2023-09-23 10:50:07 +0900

    ⚡️️ :: SelectPosition

--- presentation/src/main/java/com/onlywin/ori_android/feature/selectposition/SelectPosition.kt
+++ presentation/src/main/java/com/onlywin/ori_android/feature/selectposition/SelectPosition.kt
@@ -1,6 +1,9 @@
 package com.onlywin.ori_android.feature.selectposition
 
+import android.content.Context
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.view.LayoutInflater
+import android.view.View
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,11 +24,24 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapView
 import com.onlywin.designsystem.DuckTheme
+import com.onlywin.designsystem.button.DuckLargeButton
 import com.onlywin.designsystem.component.DuckLayout
+import com.onlywin.designsystem.fondation.color.DuckButtonColor
+import com.onlywin.designsystem.fondation.typography.Body1
 import com.onlywin.designsystem.fondation.typography.Body3
+import com.onlywin.designsystem.fondation.typography.Heading2
 import com.onlywin.designsystem.header.DuckHeader
 import com.onlywin.ori_android.R
+
+enum class PositionType {
+    START,
+    END,
+}
 
 @Composable
 internal fun SelectPosition() {
@@ -37,6 +53,17 @@ internal fun SelectPosition() {
         val start by remember { mutableStateOf("") }
         val end by remember { mutableStateOf("") }
 
+        val position by remember { mutableStateOf("대덕소프트웨어마이스터고등학교") }
+        val positionDetail by remember { mutableStateOf("대전광역시 유성구 가정북로 76") }
+
+        val onStartClick: () -> Unit = {
+
+        }
+
+        val onEndClick: () -> Unit = {
+
+        }
+
         DuckLayout {
             DuckHeader(
                 leadingIcon = com.onlywin.designsystem.R.drawable.ic_arrow_back,
@@ -45,6 +72,16 @@ internal fun SelectPosition() {
             Destinations(
                 start = start,
                 end = end,
+            )
+            AndroidView(
+                modifier = Modifier.weight(1f),
+                factory = { initMapView(it) },
+            )
+            Position(
+                position = position,
+                positionDetail = positionDetail,
+                onStartClick = onStartClick,
+                onEndClick = onEndClick,
             )
         }
     }
@@ -83,10 +120,66 @@ private fun Destinations(
     }
 }
 
-enum class PositionType {
-    START,
-    END,
+private fun initMapView(context: Context): View {
+    val view = LayoutInflater.from(context).inflate(
+        /* resource = */ R.layout.select_position,
+        /* root = */ null,
+        /* attachToRoot = */ false,
+    )
+
+    val mapView = view.findViewById<MapView>(R.id.map_view)
+    mapView.start(object : KakaoMapReadyCallback() {
+        override fun onMapReady(kakaoMap: KakaoMap) {
+
+        }
+    })
+
+    return view
 }
+
+@Composable
+private fun Position(
+    position: String,
+    positionDetail: String,
+    onStartClick: () -> Unit,
+    onEndClick: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 20.dp,
+                vertical = 16.dp,
+            ),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Heading2(text = position)
+        Body1(text = positionDetail)
+    }
+    Row(
+        modifier = Modifier.padding(
+            horizontal = 20.dp,
+            vertical = 8.dp,
+        ),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+        Box(modifier = Modifier.weight(1f)) {
+            DuckLargeButton(
+                text = stringResource(id = R.string.select_position_select_start),
+                onClick = onStartClick,
+            )
+        }
+        Box(modifier = Modifier.weight(1f)) {
+            DuckLargeButton(
+                text = stringResource(id = R.string.select_position_select_end),
+                onClick = onEndClick,
+                buttonColor = DuckButtonColor.SubColor,
+            )
+        }
+    }
+}
+
 
 @Composable
 private fun Destination(
