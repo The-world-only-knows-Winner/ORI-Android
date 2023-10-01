611e6d29cf40b319f58af1a6d8e1ab9c2c0b5590
정승훈 <hks026naver@gmail.com>
2023-10-02 01:51:38 +0900

    ♻️ :: ORIApplication koin 리팩토링

--- data/src/main/java/com/onlywin/data/utils/ApiProvider.kt
+++ data/src/main/java/com/onlywin/data/utils/ApiProvider.kt
@@ -1,14 +1,21 @@
 package com.onlywin.data.utils
 
 import com.onlywin.data.api.AuthApi
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiProvider {
-    internal fun getRetrofit() = Retrofit.Builder()
+    internal fun getRetrofit(
+        interceptor: Interceptor,
+    ) = Retrofit.Builder()
         .baseUrl("https://jeongho.dev")
+        .client(OkHttpClient.Builder().addInterceptor(interceptor).build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
 
-fun getAuthApi(): AuthApi? = ApiProvider.getRetrofit().create(AuthApi::class.java)
+fun getAuthApi(
+    interceptor: Interceptor,
+): AuthApi? = ApiProvider.getRetrofit(interceptor).create(AuthApi::class.java)
--- presentation/src/main/java/com/onlywin/ori_android/ORIApplication.kt
+++ presentation/src/main/java/com/onlywin/ori_android/ORIApplication.kt
@@ -1,12 +1,17 @@
 package com.onlywin.ori_android
 
 import android.app.Application
-import com.onlywin.data.datasource.auth.AuthDataSource
-import com.onlywin.data.datasource.auth.AuthDataSourceImpl
+import com.onlywin.data.datasource.auth.local.LocalAuthDataSource
+import com.onlywin.data.datasource.auth.local.LocalAuthDataSourceImpl
+import com.onlywin.data.datasource.auth.remote.RemoteAuthDataSource
+import com.onlywin.data.datasource.auth.remote.RemoteAuthDataSourceImpl
 import com.onlywin.data.repository.AuthRepositoryImpl
+import com.onlywin.data.utils.ORIInterceptor
 import com.onlywin.data.utils.getAuthApi
 import com.onlywin.domain.repository.AuthRepository
 import com.onlywin.ori_android.viewmodel.SignInViewModel
+import okhttp3.Interceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -15,15 +20,22 @@ class ORIApplication : Application() {
         super.onCreate()
 
         val apiModule = module {
-            single { getAuthApi() }
+            single<Interceptor> { ORIInterceptor(get()) }
+            single { getAuthApi(get()) }
         }
 
         val dataSourceModule = module {
-            single<AuthDataSource> { AuthDataSourceImpl(get()) }
+            single<RemoteAuthDataSource> { RemoteAuthDataSourceImpl(get()) }
+            single<LocalAuthDataSource> { LocalAuthDataSourceImpl(androidContext()) }
         }
 
         val repositoryModule = module {
-            single<AuthRepository> { AuthRepositoryImpl(get()) }
+            single<AuthRepository> {
+                AuthRepositoryImpl(
+                    remoteAuthDataSource = get(),
+                    localAuthDataSource = get(),
+                )
+            }
         }
 
         val viewModelModule = module {
@@ -31,6 +43,7 @@ class ORIApplication : Application() {
         }
 
         startKoin {
+            androidContext(applicationContext)
             modules(
                 listOf(
                     apiModule,
